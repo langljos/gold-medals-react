0e515689105196c796205088107064e3b0b6f279
langljos <langljos@gmail.com>
2023-03-03 16:45:14 -0600

    Changed to live endpoints

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
-// export const apiEndpoint = "https://jlanglois-olympic-medals.azurewebsites.net/api/Country";
-export const apiEndpoint = "https://localhost:7118/api/Country";
-export const hubEndpoint = "https://localhost:7118/medalsHub";
-// export const apiEndpoint = "https://jlanglois-olympic-medals.azurewebsites.net/medalsHub";
+export const apiEndpoint = "https://jlanglois-olympic-medals.azurewebsites.net/api/Country";
+// export const apiEndpoint = "https://localhost:7118/api/Country";
+// export const hubEndpoint = "https://localhost:7118/medalsHub";
+export const hubEndpoint = "https://jlanglois-olympic-medals.azurewebsites.net/medalsHub";
