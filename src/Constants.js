de1a484788dc9288ecb9d829d69fc9f3eb703e15
langljos <langljos@gmail.com>
2023-03-20 20:53:56 -0500

    Updated to live constants

--- src/Constants.js
+++ src/Constants.js
@@ -1,11 +1,8 @@
-// export const apiEndpoint = "https://jlanglois-olympic-medals.azurewebsites.net/api/Country";
-export const apiEndpoint = "https://localhost:7118/api/Country";
+export const apiEndpoint = "https://jlanglois-olympic-medals.azurewebsites.net/api/Country";
+// export const apiEndpoint = "https://localhost:7118/api/Country";
 
-// export const apiEndpoint = "https://medalsapi.azurewebsites.net/jwt/api/country";
-// export const apiEndpoint = "https://localhost:7118/jwt/api/country";
-
-// export const hubEndpoint = "https://jlanglois-olympic-medals.azurewebsites.net/medalsHub";
-export const hubEndpoint = "https://localhost:7118/medalsHub";
+export const hubEndpoint = "https://jlanglois-olympic-medals.azurewebsites.net/medalsHub";
+// export const hubEndpoint = "https://localhost:7118/medalsHub";
 
 export const usersEndpoint = "https://jlanglois-olympic-medals.azurewebsites.net/api/users/login";
 // export const usersEndpoint = "https://localhost:5001/api/users/login";
